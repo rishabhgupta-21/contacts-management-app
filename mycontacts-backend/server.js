89614dc80483a10fc6ddc6455514834cf5414004
rishabhgupta <contctrishabh@gmail.com>
2023-08-19 17:24:38 +0530

    configured express server and learnt basic routing functionality

---
+++ mycontacts-backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Home Page');
+})
+
+app.get('/api/contacts', (req, res) => {
+    res.status(200).json({ message: 'Get all contacts' });
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
